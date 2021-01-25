3bc10761e0c593ee7adec76737cd3286efe233b2
rkvarun22 <rkvarun22@gmail.com>
2021-01-25 23:09:45 +0530

    forecast update

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ request({ url, json:true }, (error, {body}) => {
     } else if (body.error) {
         callback('Unable to find location', undefined)
     } else {
-        callback(undefined, body.current.weather_descriptions[0] + ', It is Currently ' + body.current.temperature +  ' degrees out, ' +  ' It feels like ' + body.current.precip + '% chance of rain')
+        callback(undefined, body.current.weather_descriptions[0] + ', It is Currently ' + body.current.temperature +  ' degrees out, ' +  ' It feels like ' + body.current.precip + '% chance of rain. '  +  ' The humidity is ' + body.current.humidity)
     }
 
     })
