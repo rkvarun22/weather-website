c92488f74e490299e137e96187b3ba1d55b07968
rkvarun22 <rkvarun22@gmail.com>
2021-01-25 20:43:11 +0530

    remove unneccesary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,8 @@
-//const { response } = require("express")
-
-console.log('Client side javascript file is loaded!')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-//messageOne.textContent = 'From javascipt'
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
